256b44c082561e5d8ee8624cdba067374a95dc1b
hstewart38 <hstewart38@gatech.edu>
2025-07-02 13:01:23 -0400

    P2.hs, alternatives for P1.hs

--- P1.hs
+++ P1.hs
@@ -1,2 +1,6 @@
 main :: IO ()
-main = print $ sum ([x | x <- [1 .. 999], mod x 3 == 0 || mod x 5 == 0])
+main = print (sum ([x | x <- [1 .. 999], mod x 3 == 0 || mod x 5 == 0]))
+
+-- main = print $ sum $ [x | x <- [1 .. 999], mod x 3 == 0 || mod x 5 == 0]
+-- main = print $ sum $ filter (\x -> mod x 3 == 0 || mod x 5 == 0) [1 .. 999]
+-- main = print $ (sum . filter (\x -> mod x 3 == 0 || mod x 5 == 0)) [1 .. 999]
